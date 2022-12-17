bdff65d855d4b1b6702117c4c9714ff06962612f
BigBradCute <bradslater12@gmail.com>
2022-12-17 17:40:56 +0200

    Huge leaps forward
    Front end for login and home page
    Back end for messages and users
    Messages from back end displayed to the front end chat box

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "Brad", password: "password")
+User.create(name: "Gandalf", password: "password")
+User.create(name: "Frodo", password: "password")
+User.create(name: "Arya", password: "password")
+
+Message.create(body: "Oh Sam!", user_id: 3)
+Message.create(body: "I'm not a girl!", user_id: 4)
+Message.create(body: "I'll call it needle", user_id: 4)
+Message.create(body: "Fly, you fools!", user_id: 2)
+Message.create(body: "We must trust now in Frodo", user_id: 2)
+Message.create(body: "Wicked", user_id: 1)
