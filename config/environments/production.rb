ba579a9039c23e252a36fa9874f71196064065ef
BigBradCute <bradslater12@gmail.com>
2022-12-18 13:38:39 +0200

    Production Setup
    Redis GEM add
    PG production gem add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  # config.action_cable.url = 'wss://messenger.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://messenger-app.herokuapp.com', 'http://messenger-app.herokuapp.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -82,6 +82,7 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  config.web_socket_server_url = "wss://messenger-app.herokuapp.com/cable" 
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
