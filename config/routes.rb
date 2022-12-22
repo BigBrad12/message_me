da5300db3702060949af343f2dc926bf26281a26
Bradley Slater <bradslater12@gmail.com>
2022-12-22 19:44:43 +0200

    Back to where we were

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'chatroom#index'
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
   get 'login', to: 'session#new'
   post 'login', to: 'session#create'
   delete 'logout', to: 'session#destroy'
